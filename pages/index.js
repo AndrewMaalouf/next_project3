ed9a3a43d5d5acf493393ebe4c448322b35ea57c
Andrew Maalouf <andrewmaalouf11@gmail.com>
2024-10-02 16:58:00 +0300

    swiper

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,14 @@ import Footer from "@/components/Footer";
 import Form from "@/components/Form";
 import Header from "@/components/Header";
 import Services from "@/components/Services";
+import { Navigation, Pagination, Scrollbar, A11y } from 'swiper/modules';
+import { Swiper, SwiperSlide } from 'swiper/react';
 
+// Import Swiper styles
+import 'swiper/css';
+import 'swiper/css/navigation';
+import 'swiper/css/pagination';
+import 'swiper/css/scrollbar';
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -28,9 +35,9 @@ export default function Home() {
       description: "Li Europan lingues es membres del sam familie. Lor separat existentie es un myth. Por scientie, musica, sport etc, litot Europa usa li sam vocabular.",
     },
   ];
+
   return (
     <>
-
       <Navbar />
       <main>
         <Header
@@ -38,12 +45,66 @@ export default function Home() {
           title="Welcome to My Website"
           text="You can check our services and contact us!"
         />
-        <Services services={services}
-          servicesTitle={"Our Services"}
-        />
+
+        <Services services={services} servicesTitle={"Our Services"} />
         <Form />
 
-
+        {/* Swiper with images */}
+        <Swiper
+  modules={[Navigation, Pagination, Scrollbar, A11y]}
+  spaceBetween={50}
+  slidesPerView={3}
+  navigation
+  pagination={{ clickable: true }}
+  scrollbar={{ draggable: true }}
+  onSwiper={(swiper) => console.log(swiper)}
+  onSlideChange={() => console.log('slide change')}
+>
+  <SwiperSlide>
+    <div className="w-[200px] h-[200px]">
+      <Image
+        src="/img1.jpg"
+        alt="image1"
+        width={200}
+        height={200}
+        className="object-cover w-full h-full"
+      />
+    </div>
+  </SwiperSlide>
+  <SwiperSlide>
+    <div className="w-[200px] h-[200px]">
+      <Image
+        src="/img2.jpg"
+        alt="image2"
+        width={200}
+        height={200}
+        className="object-cover w-full h-full"
+      />
+    </div>
+  </SwiperSlide>
+  <SwiperSlide>
+    <div className="w-[200px] h-[200px]">
+      <Image
+        src="/img3.jpg"
+        alt="image3"
+        width={200}
+        height={200}
+        className="object-cover w-full h-full"
+      />
+    </div>
+  </SwiperSlide>
+  <SwiperSlide>
+    <div className="w-[200px] h-[200px]">
+      <Image
+        src="/img4.jpg"
+        alt="image4"
+        width={300}
+        height={200}
+        className="object-cover w-full h-full"
+      />
+    </div>
+  </SwiperSlide>
+</Swiper>
       </main>
       <Footer />
     </>
